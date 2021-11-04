d1ea261a6e13fdddd059908d5a27f29f0a782acc
Enes Zulfikaroglu <enes.zulfikaroglu34@gmail.com>
2021-11-04 14:42:01 +0300

    Cache Feature

---
+++ WebAPI/Installers/CacheInstaller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Core.Utilities.Cache;
+
+namespace WebAPI.Installers
+{
+    public class CacheInstaller : IInstaller
+    {
+        private RedisCacheSettings _redisCacheSettings;
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            var redisCacheSettings = new RedisCacheSettings();
+            _redisCacheSettings = configuration.GetSection("RedisCacheSettings").Get<RedisCacheSettings>();
+            services.AddSingleton(_redisCacheSettings);
+
+            if (!_redisCacheSettings.Enabled)
+            {
+                return;
+            }
+
+            services.AddStackExchangeRedisCache(options => options.Configuration = redisCacheSettings.ConnectionString);
+            //services.AddSingleton<IResponseCacheService, ResponseCacheService>();
+        }
+    }
+}
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -30,12 +30,11 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            /* Redis Configuration
-            
+            // Redis Configuration
             services.AddStackExchangeRedisCache(options =>
             {
-                options.Configuration = Configuration.GetValue<string>("CacheSettings: ConnectionString");
-            });*/
+                options.Configuration = Configuration.GetValue<string>("RedisCacheSettings: ConnectionString");
+            });
 
             services.AddControllers();
             services.AddCors(options =>
